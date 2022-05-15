924834c4bfc83adc8f92933260c5850cc694466d
Jason <2270620101@qq.com>
2022-05-15 22:53:30 +0800

    搭建Eureka

--- cloud-eureka-server/src/main/java/com/Jason/EurekaMain.java
+++ cloud-eureka-server/src/main/java/com/Jason/EurekaMain.java
@@ -3,17 +3,17 @@ package com.Jason;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 /**
  * @Author Jason
- * @Date 2022/5/1 20:41
+ * @Date 2022/5/15 15:53
  * @Version 1.0
  */
-
 @SpringBootApplication
-@EnableEurekaClient
-public class PaymentMain {
+@EnableEurekaServer
+public class EurekaMain {
     public static void main(String[] args) {
-        SpringApplication.run(PaymentMain.class, args);
+        SpringApplication.run(EurekaMain.class, args);
     }
 }
